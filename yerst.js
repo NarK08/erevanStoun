b5484bd4f21cfca7ee36562a5ea8c032dc0e3a26
NarK08 <122155381+NarK08@users.noreply.github.com>
2023-04-19 23:06:22 +0400

    add Project

---
+++ yerst.js
@@ -0,0 +1,28 @@
+let slides = document.getElementsByClassName("slide");
+let navlinks = document.getElementsByClassName("navlink");
+let currentSlide = 0;
+
+document.getElementById("next").addEventListener("click", () => {
+  changeSlide(currentSlide + 1);
+});
+document.getElementById("prev").addEventListener("click", () => {
+  changeSlide(currentSlide - 1);
+});
+
+function changeSlide(moveTo) {
+  if (moveTo >= slides.length) moveTo = 0;
+  if (moveTo < 0) moveTo = slides.length - 1;
+
+  slides[currentSlide].classList.toggle("active");
+  navlinks[currentSlide].classList.toggle("active");
+  slides[moveTo].classList.toggle("active");
+  navlinks[moveTo].classList.toggle("active");
+
+  currentSlide = moveTo;
+}
+
+document.querySelectorAll(".navlink").forEach((bullet, bulletIndex) => {
+  bullet.addEventListener("click", () => {
+    if (currentSlide !== bulletIndex) changeSlide(bulletIndex);
+  });
+});
